b1b2b263ab8643cbf45d2b1bc654f136d7187e8f
Rafael Ventura <raf4ael@gmail.com>
2025-01-23 12:30:16 -0300

    a

--- src/modules/web/vai-bandeijar/src/main.ts
+++ src/modules/web/vai-bandeijar/src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
@@ -11,12 +11,12 @@ import { InputTextModule } from 'primeng/inputtext';
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
-      BrowserModule,
       FormsModule,
       ButtonModule,
       CardModule,
       InputTextModule
     ),
+    provideHttpClient(), // Use este provedor em vez de HttpClientModule
     provideAnimations()
   ]
 }).catch(err => console.error(err));
