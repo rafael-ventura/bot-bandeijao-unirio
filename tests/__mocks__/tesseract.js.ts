73100defd81ce765cd7332e5a0de99fe38a0ed2c
Rafael Ventura <raf4ael@gmail.com>
2025-01-27 12:54:09 -0300

    Ajustes do codigo.

--- tests/__mocks__/tesseract.js.ts
+++
@@ -1,4 +0,0 @@
-export const createWorker = jest.fn().mockImplementation(() => ({
-  recognize: jest.fn().mockResolvedValue({ data: { text: 'Mocked OCR text' } }),
-  terminate: jest.fn().mockResolvedValue(undefined)
-})); 
